6dd09f794632cab90a2fab16e2f9a6a864d3c272
Manish Gupta <manish.gupta09-88@aapastech.com>
2022-07-20 13:34:31 +0530

    Used  absolute path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './app';
-import './styles/index.scss';
+import App from 'app';
+import 'styles/index.scss';
 
 import * as serviceWorker from './serviceWorker';
 
